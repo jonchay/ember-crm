12a1bfddfa071744afe3363297d53146355c8611
Jonathan Chay <jonchay@gmail.com>
2014-06-25 00:55:14 -0700

    Initial commit

---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,7 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+App.Router.map ()->
+  location: 'auto'
+  rootURL: '/'
+  # @resource('posts')
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberCrm::Application.config.secret_key_base = '8402ca0e124eb0100b872b802086ccbe613c3cede9b4874e08e71f4cd90567b46f62b194485202cea1abc997e06fb0f5568e5a3aff57874e27ebb403139e0be5'
